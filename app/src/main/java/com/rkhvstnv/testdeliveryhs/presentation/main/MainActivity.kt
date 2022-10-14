dc7a1ee13e879a7f9b1dbb2fac44ff94f8aa291a
r-khvstnv <r.khvastunov@gmail.com>
2022-10-14 17:40:43 +0500

    Added and implemented basic app styles
    Added:
    - fonts
    - colors
    - icons
    
    Updated:
    - theme
    
    Implemented new styles in corresponding layouts

--- app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/main/MainActivity.kt
+++ app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/main/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,10 +23,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_menu, R.id.navigation_profile, R.id.navigation_cart
