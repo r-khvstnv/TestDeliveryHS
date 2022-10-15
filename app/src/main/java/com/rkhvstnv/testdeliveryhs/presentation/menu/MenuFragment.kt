8effdf5c3aae2658e7335e22210877427f88595e
r-khvstnv <r.khvastunov@gmail.com>
2022-10-15 17:14:13 +0500

    Created and added implementation of RecyclerView's Adapter

--- app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuFragment.kt
+++ app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuFragment.kt
@@ -4,32 +4,81 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rkhvstnv.testdeliveryhs.databinding.FragmentMenuBinding
+import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.GoodsAdapter
+import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.GoodsCategoriesAdapter
+import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.PromoBannersAdapter
 
 class MenuFragment : Fragment() {
 
     private var _binding: FragmentMenuBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<MenuViewModel>()
+    private val adapterPromoBanners by lazy { PromoBannersAdapter(requireContext()) }
+    private val adapterGoodsCategories by lazy { GoodsCategoriesAdapter(requireContext()) }
+    private val adapterGoods by lazy { GoodsAdapter(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val menuViewModel =
-            ViewModelProvider(this).get(MenuViewModel::class.java)
-
         _binding = FragmentMenuBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        return root
+        setupPromoBannersRecyclerView()
+        setupGoodsCategoriesRecyclerView()
+        setupGoodsRecyclerView()
+
+        viewModel.banners.observe(viewLifecycleOwner){
+            list ->
+            if (!list.isNullOrEmpty())
+                adapterPromoBanners.submitList(list)
+        }
+    }
+
+    private fun setupPromoBannersRecyclerView(){
+        binding.rvBanners.apply {
+            adapter = adapterPromoBanners
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setupGoodsCategoriesRecyclerView(){
+        binding.rvGoodsCategories.apply {
+            adapter = adapterGoodsCategories
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setupGoodsRecyclerView(){
+        binding.rvGoods.apply {
+            adapter = adapterGoods
+            layoutManager = LinearLayoutManager(
+                requireContext(),
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+            setHasFixedSize(true)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuViewModel.kt
+++ app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuViewModel.kt
@@ -1,13 +1,9 @@
 package com.rkhvstnv.testdeliveryhs.presentation.menu
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.rkhvstnv.testdeliveryhs.domain.PromoBannersTest
 
 class MenuViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    val banners = MutableLiveData(PromoBannersTest.getPromoBannersParam())
 }
