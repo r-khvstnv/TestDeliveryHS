a5624193fe760b4267ef1c1ffdc269222042f3c4
r-khvstnv <r.khvastunov@gmail.com>
2022-10-16 17:43:43 +0500

    Draft realisation

--- app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuFragment.kt
+++ app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuFragment.kt
@@ -1,26 +1,44 @@
 package com.rkhvstnv.testdeliveryhs.presentation.menu
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rkhvstnv.testdeliveryhs.TestDeliveryHsApplication
 import com.rkhvstnv.testdeliveryhs.databinding.FragmentMenuBinding
+import com.rkhvstnv.testdeliveryhs.domain.models.GoodsCategoryParam
+import com.rkhvstnv.testdeliveryhs.domain.models.GoodsParamState
+import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.CategoriesCallback
 import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.GoodsAdapter
 import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.GoodsCategoriesAdapter
 import com.rkhvstnv.testdeliveryhs.presentation.menu.adapters.PromoBannersAdapter
+import javax.inject.Inject
 
 class MenuFragment : Fragment() {
 
     private var _binding: FragmentMenuBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel by viewModels<MenuViewModel>()
+    @Inject
+    internal lateinit var menuViewModelFactory: dagger.Lazy<MenuViewModel.Factory>
+
+    private val viewModel by viewModels<MenuViewModel>{ menuViewModelFactory.get() }
     private val adapterPromoBanners by lazy { PromoBannersAdapter(requireContext()) }
-    private val adapterGoodsCategories by lazy { GoodsCategoriesAdapter(requireContext()) }
+    private lateinit var adapterGoodsCategories: GoodsCategoriesAdapter
     private val adapterGoods by lazy { GoodsAdapter(requireContext()) }
+
+    override fun onAttach(context: Context) {
+        (requireActivity().applicationContext as TestDeliveryHsApplication)
+            .appComponet
+            .inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,9 +57,22 @@ class MenuFragment : Fragment() {
         setupGoodsRecyclerView()
 
         viewModel.banners.observe(viewLifecycleOwner){
-            list ->
+                list ->
             if (!list.isNullOrEmpty())
                 adapterPromoBanners.submitList(list)
+        }
+        viewModel.categories.observe(viewLifecycleOwner){
+                list ->
+            if (!list.isNullOrEmpty())
+                adapterGoodsCategories.submitList(null)
+                adapterGoodsCategories.submitList(list)
+        }
+        viewModel.goodsState.observe(viewLifecycleOwner){
+                state ->
+            when(state){
+                is GoodsParamState.Success -> adapterGoods.submitList(state.goodsParamList)
+                is GoodsParamState.Error -> Toast.makeText(requireContext(), state.message, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -58,6 +89,17 @@ class MenuFragment : Fragment() {
     }
 
     private fun setupGoodsCategoriesRecyclerView(){
+        adapterGoodsCategories = GoodsCategoriesAdapter(
+            requireContext(),
+            object : CategoriesCallback{
+                override fun onItemClick(goodsCategoryParam: GoodsCategoryParam) {
+                    viewModel.requestGoodsByType(goodsCategoryParam = goodsCategoryParam)
+                }
+
+            }
+        )
+
+
         binding.rvGoodsCategories.apply {
             adapter = adapterGoodsCategories
             layoutManager = LinearLayoutManager(
--- app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuViewModel.kt
+++ app/src/main/java/com/rkhvstnv/testdeliveryhs/presentation/menu/MenuViewModel.kt
@@ -1,9 +1,81 @@
 package com.rkhvstnv.testdeliveryhs.presentation.menu
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.rkhvstnv.testdeliveryhs.domain.PromoBannersTest
+import android.util.Log
+import androidx.lifecycle.*
+import com.rkhvstnv.testdeliveryhs.domain.models.GoodsCategoryParam
+import com.rkhvstnv.testdeliveryhs.domain.models.GoodsParam
+import com.rkhvstnv.testdeliveryhs.domain.models.GoodsParamState
+import com.rkhvstnv.testdeliveryhs.domain.models.PromoBannerParam
+import com.rkhvstnv.testdeliveryhs.domain.usecases.GetAllGoodsCategoriesUseCase
+import com.rkhvstnv.testdeliveryhs.domain.usecases.GetGoodsByTypeUseCase
+import com.rkhvstnv.testdeliveryhs.domain.usecases.GetPromoBannersUseCase
+import com.rkhvstnv.testdeliveryhs.utils.coroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
-class MenuViewModel : ViewModel() {
-    val banners = MutableLiveData(PromoBannersTest.getPromoBannersParam())
+class MenuViewModel (
+    private val getPromoBannersUseCase: GetPromoBannersUseCase,
+    private val getAllGoodsCategoriesUseCase: GetAllGoodsCategoriesUseCase,
+    private val getGoodsByTypeUseCase: GetGoodsByTypeUseCase
+) : ViewModel() {
+
+    class Factory @Inject constructor(
+        private val getPromoBannersUseCase: Provider<GetPromoBannersUseCase>,
+        private val getAllGoodsCategoriesUseCase: Provider<GetAllGoodsCategoriesUseCase>,
+        private val getGoodsByTypeUseCase: Provider<GetGoodsByTypeUseCase>
+    ): ViewModelProvider.Factory{
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MenuViewModel(
+                getPromoBannersUseCase = getPromoBannersUseCase.get(),
+                getAllGoodsCategoriesUseCase = getAllGoodsCategoriesUseCase.get(),
+                getGoodsByTypeUseCase = getGoodsByTypeUseCase.get()
+            ) as T
+        }
+    }
+
+
+
+
+    val banners = MutableLiveData<List<PromoBannerParam>>()
+    val categories = MutableLiveData<List<GoodsCategoryParam>>()
+    val goodsState = MutableLiveData<GoodsParamState>()
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData(){
+        banners.postValue(getPromoBannersUseCase.execute())
+        val cat = getAllGoodsCategoriesUseCase.execute()
+        cat[0].isChecked = true
+        categories.postValue(cat)
+
+        /*viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler){
+            val gds = getGoodsByTypeUseCase.execute(cat[0])
+            goodsState.postValue(gds)
+        }*/
+    }
+
+
+    fun requestGoodsByType(goodsCategoryParam: GoodsCategoryParam){
+        categories.value?.let {
+            list ->
+            list.map {
+                if (it.isChecked && it != goodsCategoryParam) it.isChecked = false
+
+                if (it == goodsCategoryParam) goodsCategoryParam.isChecked = true
+            }
+            categories.postValue(list)
+        }
+
+        /*viewModelScope.launch(Dispatchers.IO + coroutineExceptionHandler){
+            val gds = getGoodsByTypeUseCase.execute(goodsCategoryParam)
+            goodsState.postValue(gds)
+        }*/
+    }
+
+
 }
